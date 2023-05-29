9c22bf66420b72d02f5c6eef42d88e4b50fdcb90
Brenda <brentorres26@gmail.com>
2023-05-29 08:54:08 -0300

    refactorizacion de codigo

--- casanova/src/main/java/hotel/casanova/PruebaController.java
+++ casanova/src/main/java/hotel/casanova/PruebaController.java
@@ -1,6 +1,5 @@
 package hotel.casanova;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- casanova/src/main/java/hotel/casanova/model/Huesped.java
+++ casanova/src/main/java/hotel/casanova/model/Huesped.java
@@ -5,12 +5,9 @@ import java.sql.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
