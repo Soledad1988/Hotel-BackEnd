6d272b540f7be7575f8f9c14bc0c01c97689748b
Brenda <brentorres26@gmail.com>
2023-05-29 08:47:11 -0300

    Controller

---
+++ casanova/src/main/java/hotel/casanova/PruebaController.java
@@ -0,0 +1,16 @@
+package hotel.casanova;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prueba")
+public class PruebaController {
+
+	@PostMapping
+	public String prueba() {
+		return "se logra conexión";
+	}
+}
---
+++ casanova/src/main/java/hotel/casanova/controller/HuespedController.java
@@ -0,0 +1,59 @@
+package hotel.casanova.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import hotel.casanova.model.Huesped;
+import hotel.casanova.service.HuespedService;
+
+
+@RestController
+@CrossOrigin(origins="https://localhost:4200")
+@RequestMapping("/huespedes")
+public class HuespedController {
+
+	@Autowired
+	private HuespedService huespedService;
+	//private HuespedRepository huespedRepository;
+
+	@PostMapping("")
+    public void agregarHuesped(@RequestBody Huesped huesped){
+		huespedService.crearHuesped(huesped);
+    }
+    
+    @GetMapping("")
+    @ResponseBody
+    public List<Huesped> verHuesped (){
+       return huespedService.verHuesped();
+    }
+    
+     //obtiene
+    @GetMapping("/{id}")
+    public Huesped get(@PathVariable("id") Integer id){
+       return huespedService.buscarHuesped(id);
+    }
+   
+	 @DeleteMapping("/{id}")
+	 void borrarHuesped(@PathVariable Integer id){
+		   huespedService.borrarHuesped(id);
+	 }
+  
+  
+	  @PutMapping("")
+	  public void actualizar(@RequestBody Huesped huesped){
+		   huespedService.actualizar(huesped);
+	  }
+	
+	
+}
