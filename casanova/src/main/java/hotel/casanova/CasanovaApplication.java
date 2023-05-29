618449ac17b62051830d458ce45a22358a2a5e84
Brenda <brentorres26@gmail.com>
2023-05-29 08:38:24 -0300

    Inicio proyecto

---
+++ casanova/src/main/java/hotel/casanova/CasanovaApplication.java
@@ -0,0 +1,36 @@
+package hotel.casanova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class CasanovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasanovaApplication.class, args);
+		
+		//new Relaciones().iniciar();
+	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                    @Override
+public void addCorsMappings(CorsRegistry corsRegistry) {
+corsRegistry.addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .exposedHeaders("Authorization")
+        .allowCredentials(true).maxAge(3600);
+}
+   
+            };
+    }   
+	
+		
+
+}
---
+++ casanova/src/test/java/hotel/casanova/CasanovaApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel.casanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasanovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
